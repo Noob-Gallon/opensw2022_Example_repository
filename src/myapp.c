e58eb3785089ceff888d73a71378c65d8a8a8f75
jdk <inujdk@inu.ac.kr>
2022-11-03 17:53:21 +0900

    comments are written

--- src/myapp.c
+++ src/myapp.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 int main(void) {
-
+    // call two functions 3 times...
     for (int i = 0; i < 3; i++) {
 
         foo();
         goo();
     }
 
+    // call zoo three times...
     for (int i = 0; i < 3; i++) {
 
         zoo();
